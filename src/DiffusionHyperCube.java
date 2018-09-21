93153af724bf1cfa0710ffb00d3ce66ea2c8c8bf
TrimA <florianseb96@hotmail.fr>
2018-09-21 10:36:40 +0200

    broadcast hypercube generic done

--- src/DiffusionHyperCube.java
+++ src/DiffusionHyperCube.java
@@ -6,30 +6,33 @@ public class DiffusionHyperCube {
         MPI.Init(args);
         int me = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
-        int idBroadCaster = 0;
-        Object buffer[] = new Object[2];
 
+        int idBroadCaster = 0;
+        if(args[3].equals("-broadcaster")){
+            idBroadCaster = Integer.parseInt(args[4]);
+        }
+        // init buffer
+        String buffer[] = new String[2];
         if(me == idBroadCaster){
             buffer[0] = "coucou";
-            buffer[1] = 0;
-            System.out.println("I'm <"+me+">: send " + buffer[0] + " to " + ((me+1) % size));
-            MPI.COMM_WORLD.Send(buffer, 0, 1, MPI.OBJECT, (me +1) % size , 99);
-        }else{
-            Status mps = MPI.COMM_WORLD.Recv( buffer, 0, 1, MPI.OBJECT, MPI.ANY_SOURCE, 99 );
+            buffer[1] = "0";
+            System.out.println("I'm <"+me+">: send " + buffer[0] + " to " + (me +1 % size));
+            MPI.COMM_WORLD.Send(buffer, 0, 2, MPI.OBJECT, me + 1 % size, 99);
+
+        } else {
+            Status mps = MPI.COMM_WORLD.Recv( buffer, 0, 2, MPI.OBJECT, MPI.ANY_SOURCE, 99 );
             System.out.println("I'm <"+me+">: receive " + buffer[0] + " state" + buffer[1]);
-            if(me +1 <= size/2){
-                System.out.println("I'm <"+me+">: send " + buffer[0] + " to " + (me +1) );
-                MPI.COMM_WORLD.Send(buffer, 0, 1, MPI.OBJECT, me +1, 99);
+        }
+        double state = Double.parseDouble(buffer[1]);
+        while(Math.pow(2,state +1 ) -1 < size){
+            state++;
+            buffer[1]=Double.toString(state);
+            if(( (me - idBroadCaster + size) % size ) + Math.pow(2,state) < size){
+                System.out.println("I'm <"+me+">: send " + buffer[0] + " to " + (int) (me + Math.pow(2,state)) % size);
+                MPI.COMM_WORLD.Send(buffer, 0, 2, MPI.OBJECT, (int) (me + Math.pow(2,state)) % size , 99);
             }
-        } /*
-        else {
-            Status mps = MPI.COMM_WORLD.Recv( buffer, 0, 1, MPI.OBJECT,MPI.ANY_SOURCE , 99 );
-            System.out.println("I'm <"+me+">: receive " + buffer[0]);
-            if(me > size/2){
-                System.out.println("I'm <"+me+">: send " + buffer[0] + " to " + (((me+1) %size) -1) );
-                MPI.COMM_WORLD.Send(buffer, 0, 1, MPI.OBJECT, ((me+1) %size) -1, 99);
-            }
-        }*/
+
+        }
         MPI.Finalize();
     }
 }
--- src/run.bat
+++ src/run.bat
@@ -1,2 +1,2 @@
 javac -cp %MPJ_HOME%/lib/mpj.jar %1.java
-mpjrun.bat -np %2 %1
+mpjrun.bat -np %2 %1 -broadcaster %3
